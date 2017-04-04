1bd8c146888aa1c34a18da21240631d276efa9dd
Andrii Dmytrenko <andrii.dmytrenko@fundingcircle.com>
2017-04-04 14:18:30 +0100

    Add flags to list required env variables and require all.

--- src/main.rs
+++ src/main.rs
@@ -10,33 +10,61 @@ use std::env;
 use std::collections::HashMap;
 use std::iter::FromIterator;
 
-fn template<'t>(input: &'t [u8]) -> ::std::borrow::Cow<'t, [u8]> {
+fn template<'t>(input: &'t [u8]) -> (::std::borrow::Cow<'t, [u8]>, bool) {
     lazy_static! {
         static ref RE: Regex = Regex::new(r"\$\{(\w+)\}").unwrap();
         static ref ENV: HashMap<String, String> = HashMap::from_iter(env::vars());
     }
-    RE.replace_all(input, |caps: &Captures| {
+    let mut replaced_all = true;
+    let result = RE.replace_all(input, |caps: &Captures| {
         let var_name = caps.get(1).unwrap().as_bytes();
         let stringified_name = String::from_utf8_lossy(var_name).into_owned();
-        ENV.get(&stringified_name).map(|x| x.as_bytes()).unwrap_or(caps.get(0).unwrap().as_bytes()).to_vec()
-    })
+        ENV.get(&stringified_name).map(|x| x.as_bytes()).unwrap_or_else(|| {
+            replaced_all = false;
+            caps.get(0).unwrap().as_bytes()
+        }).to_vec()
+    });
+    (result, replaced_all)
+}
+
+fn list_matches(input: &[u8]) -> Vec<String> {
+    lazy_static! {
+        static ref RE: Regex = Regex::new(r"\$\{(\w+)\}").unwrap();
+    }
+    Vec::from_iter(RE.captures_iter(input).map(|caps: Captures|
+        String::from_utf8_lossy(caps.get(1).unwrap().as_bytes()).into_owned()
+    ))
 }
 
 fn main() {
-  let matches = App::new(env!("CARGO_PKG_NAME"))
-    .version(env!("CARGO_PKG_VERSION"))
-    .author("Andrii Dmytrenko <andrii@dmytrenko.uk>")
-    .about("templates files with environment variables")
-    .arg(Arg::with_name("input").index(1).default_value("/dev/stdin"))
-    .arg(Arg::with_name("output").index(2).default_value("/dev/stdout"))
-    .get_matches();
+    let matches = App::new(env!("CARGO_PKG_NAME"))
+        .version(env!("CARGO_PKG_VERSION"))
+        .author("Andrii Dmytrenko <andrii@dmytrenko.uk>")
+        .about("templates files with environment variables")
+        .arg(Arg::with_name("list").long("list").short("l").help("List required environment variables"))
+        .arg(Arg::with_name("require-all").long("require-all").short("r").help("Fail if not all enrionent variables available"))
+        .arg(Arg::with_name("input").default_value("/dev/stdin"))
+        .arg(Arg::with_name("output").default_value("/dev/stdout"))
+        .get_matches();
 
-  let input_file_name = matches.value_of("input").unwrap();
-  let output_file_name = matches.value_of("output").unwrap();
-  let mut f1 = File::open(input_file_name).expect("Can't open input file");
-  let mut f2 = File::create(output_file_name).expect("Can't open output file");
-  let mut input = Vec::with_capacity(4_194_304);
-  f1.read_to_end(&mut input).expect("Can't read input file");
-  let result = template(&input);
-  f2.write_all(result.as_ref()).expect("Can't write output file");
+    let input_file_name = matches.value_of("input").unwrap();
+    let output_file_name = matches.value_of("output").unwrap();
+    let mut f1 = File::open(input_file_name).expect("Can't open input file");
+    let mut f2 = File::create(output_file_name).expect("Can't open output file");
+
+    let mut input = Vec::with_capacity(4_194_304);
+    f1.read_to_end(&mut input).expect("Can't read input file");
+
+    if matches.is_present("list") {
+        for m in list_matches(&input) {
+            f2.write_fmt(format_args!("{}\n", m)).unwrap();
+        }
+    } else {
+        let (result, replaced_all) = template(&input);
+        if matches.is_present("require-all") && !replaced_all {
+            drop(f2);
+            ::std::process::exit(1);
+        }
+        f2.write_all(result.as_ref()).expect("Can't write output file");
+    }
 }
